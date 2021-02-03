510cec4545bcca37039877f00579bceb28383314
alannatran <atran.art@gmail.com>
2021-02-03 15:21:54 -0800

    created background, created circle with mouse interactivity

---
+++ js/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  // put setup code here to run once
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+
+
+
+}
+
+function draw() {
+  // put drawing code here to run in a loop
+
+  circle(mouseX,mouseY,200);
+  fill(90, 200, 190, 80);
+
+
+
+}
